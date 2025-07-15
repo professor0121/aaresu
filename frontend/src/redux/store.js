306064d8a6cd80d2c1c541dc174a9770cb77f604
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-07-15 16:45:55 +0530

    ui setup is done using react+tailwinds+redux+axios

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,11 @@
+// redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './rootReducer';
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+export default store;
